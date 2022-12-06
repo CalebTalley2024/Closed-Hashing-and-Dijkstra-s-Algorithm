5326ca8fbad3142447b5d5f50be7f2969d4ac1f5
Caleb Talley <86822049+CalebTalley2024@users.noreply.github.com>
2022-12-06 18:14:09 -0500

    started project
    Problem solved: getting info from txt file
    solution: (something was wrong with the txt file that I downloaded, so I just made a txt file from scratch and pasted to content using the preview on the website)
    
    Problems: Q1-3:
    
    make sure to add a scanner to Q4 when finished
    
    Q4: have basic concept (using recursion, but the output is wrong)

--- ReadMe.md
+++ ReadMe.md
@@ -9,3 +9,6 @@
 # Q4: Dijsktra's Algorithm
 # Q4 ~ 4-5 hrs
 - iterate through every option till you find the best path
+
+
+Hash table cannot have null vals while hashmap can
--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,44 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import static java.nio.file.Files.readString;
 
 public class Main {
 
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
 
-//        File file = new File("EdgarAllanPoeBellsB2022groomed.txt");
-//        System.out.println(file.canRead());
-//        Scanner scan = new Scanner(file);
-//        System.out.println(scan.nextLine());
+        File file = new File("EdgarAllanPoeBellsB2022groomed.txt");
+
+        String currentPath = file.getAbsolutePath();
+        System.out.println(currentPath);
+        System.out.println(file.canRead());
+        Scanner scan = new Scanner(file);
+
+        while(scan.hasNext()){
+            String word = scan.next();
+            System.out.println(word);
+
+        }
+        scan.close();
+//        System.out.println(file);
+
+
+
+
 
         // Question 4
 
-        int[][] adj = Graph.adjMatrix;
-        // test trying to get to D(9)
-        ArrayList<Integer> path = new ArrayList<>();
-        Graph.dijkstra(adj,0,9,path);
+//        int[][] adj = Graph.adjMatrix;
+//        // test trying to get to D(9)
+//        ArrayList<Integer> path = new ArrayList<>();
+//        Graph.dijkstra(adj,0,9,path);
 
 
     }
